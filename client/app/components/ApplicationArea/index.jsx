91bb777a7132d64e01d7e2dfcbacbe1b016d3c20
Carylearner <47635236+Carylearner@users.noreply.github.com>
2020-04-08 16:26:51 +0800

    Update index.jsx

--- client/app/components/ApplicationArea/index.jsx
+++ client/app/components/ApplicationArea/index.jsx
@@ -23,6 +23,13 @@ export default function ApplicationArea() {
     document.body.addEventListener("click", handleNavigationIntent, false);
     window.addEventListener("error", globalErrorHandler, false);
 
+    /*
+    事件捕获，从dom结构上到下，window.addEventListener先于document.addEventListener被捕获事件冒泡，
+    从dom结构底层向上传递，document.addEventListener先于window.addEventListener被触发
+    addEventListener函数的最后一个布尔型参数 true - 事件句柄在捕获阶段执行 false- 默认, 事件句柄在冒泡阶段执行
+    参见https://www.runoob.com/jsref/dom-obj-event.html文档
+    */
+    
     return () => {
       document.body.removeEventListener("click", handleNavigationIntent, false);
       window.removeEventListener("error", globalErrorHandler, false);
