f46822457a504238e6a616feb7da4baba3cc6cd1
Carylearner <47635236+Carylearner@users.noreply.github.com>
2020-04-21 17:49:32 +0800

    注释代码

--- client/app/index.js
+++ client/app/index.js
@@ -6,6 +6,7 @@ import "@/config";
 
 import offlineListener from "@/services/offline-listener";
 
+//ReactDOM.render(element, container[, callback]) 如果提供了可选的回调函数，该回调将在组件被渲染或更新之后被执行。
 ReactDOM.render(<ApplicationArea />, document.getElementById("application-root"), () => {
   offlineListener.init();
 });
